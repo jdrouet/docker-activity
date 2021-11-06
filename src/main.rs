8ece15b8c272a1e0178ce374f49f38eff3f28d0e
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-11-06 20:59:04 +0100

    feat: handle watching several containers
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use bollard::container::{Stats, StatsOptions};
+use bollard::models::SystemEventsResponse;
+use bollard::system::EventsOptions;
 use bollard::Docker;
 use clap::Parser;
 use futures_util::stream::StreamExt;
 use powercap::PowerCap;
+use std::collections::{HashMap, HashSet};
+use std::convert::TryFrom;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
-use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
 fn both<A, B>(a: Option<A>, b: Option<B>) -> Option<(A, B)> {
     if let (Some(a), Some(b)) = (a, b) {
@@ -58,31 +62,38 @@ impl Snapshot {
     }
 }
 
-#[derive(Parser)]
-struct Application {
-    #[clap(about = "Name or ID of the container to monitor")]
-    container: String,
-    #[clap(about = "Path to write the file")]
-    output: PathBuf,
+struct ContainerWatcher {
+    docker: Arc<Docker>,
+    powercap: Arc<Option<PowerCap>>,
+    name: String,
+    file: File,
 }
 
-impl Application {
-    fn write_header(&self, file: &mut File) -> Result<(), Box<dyn std::error::Error>> {
-        writeln!(
+impl ContainerWatcher {
+    async fn execute(
+        docker: Arc<Docker>,
+        powercap: Arc<Option<PowerCap>>,
+        register: Arc<Mutex<HashSet<String>>>,
+        name: String,
+        path: PathBuf,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let file = File::create(&path)?;
+        let mut watcher = ContainerWatcher {
+            docker,
+            powercap,
+            name,
             file,
-            "ts,pid_count,pid_limit,memory_usage,memory_limit,cpu_percent,cpu_count,cpu_energy",
-        )?;
-        Ok(())
+        };
+        watcher.run(register).await
     }
 
-    async fn process_stream(
-        &self,
-        file: &mut File,
-        docker: &Docker,
-        powercap: Option<&PowerCap>,
+    async fn run(
+        &mut self,
+        register: Arc<Mutex<HashSet<String>>>,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        let stream = &mut docker.stats(
-            &self.container,
+        println!("watching container={}", self.name);
+        let stream = &mut self.docker.stats(
+            &self.name,
             Some(StatsOptions {
                 stream: true,
                 one_shot: false,
@@ -90,12 +101,16 @@ impl Application {
         );
         let mut last_energy: Option<u64> = None;
         while let Some(Ok(stat)) = stream.next().await {
-            let energy = powercap.and_then(|pcap| pcap.intel_rapl.total_energy().ok());
+            let energy = self
+                .powercap
+                .as_ref()
+                .as_ref()
+                .and_then(|pcap| pcap.intel_rapl.total_energy().ok());
             let delta = both(last_energy, energy).map(|(prev, next)| next - prev);
             last_energy = energy;
             let snap = Snapshot::build(stat, delta);
             writeln!(
-                file,
+                self.file,
                 "{},{},{},{},{},{},{},{}",
                 snap.ts,
                 snap.pid_count.unwrap_or_default(),
@@ -107,25 +122,203 @@ impl Application {
                 snap.cpu_energy.unwrap_or_default(),
             )?;
         }
-        Ok(())
-    }
 
-    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut file = File::create(&self.output).expect("Couldn't create file");
-        let docker =
-            Docker::connect_with_local_defaults().expect("Couldn't connect to docker engine");
-        let powercap = PowerCap::try_default().ok();
-        self.write_header(&mut file)?;
-        loop {
-            self.process_stream(&mut file, &docker, powercap.as_ref())
-                .await?;
-            tokio::time::sleep(Duration::new(1, 0)).await;
+        let mut lock = register.try_lock();
+        if let Ok(ref mut mutex) = lock {
+            mutex.remove(&self.name);
+            println!("done watching container={}", self.name);
+        } else {
+            eprintln!("couldn't unregister container={}", self.name);
         }
+        Ok(())
     }
 }
 
+fn get_container_name(event: &SystemEventsResponse) -> Option<String> {
+    event
+        .actor
+        .as_ref()
+        .and_then(|actor| actor.attributes.as_ref())
+        .and_then(|attrs| attrs.get("name"))
+        .cloned()
+}
+
+struct Orchestrator {
+    docker: Arc<Docker>,
+    powercap: Arc<Option<PowerCap>>,
+    names: HashSet<String>,
+    path: PathBuf,
+    tasks: Arc<Mutex<HashSet<String>>>,
+}
+
+impl TryFrom<Params> for Orchestrator {
+    type Error = Box<dyn std::error::Error>;
+
+    fn try_from(value: Params) -> Result<Self, Self::Error> {
+        let docker = Arc::new(Docker::connect_with_local_defaults()?);
+        let powercap = Arc::new(PowerCap::try_default().ok());
+        Ok(Self {
+            docker,
+            powercap,
+            names: value
+                .containers
+                .map(|value| value.split(',').map(String::from).collect())
+                .unwrap_or_default(),
+            path: value.output,
+            tasks: Arc::new(Mutex::new(HashSet::new())),
+        })
+    }
+}
+
+impl Orchestrator {
+    fn is_running(&self, name: &String) -> bool {
+        self.tasks
+            .lock()
+            .map(|lock| lock.contains(name))
+            .unwrap_or(false)
+    }
+
+    fn register_task(&self, name: String) {
+        self.tasks
+            .lock()
+            .map(|mut lock| lock.insert(name))
+            .expect("mutex is corrupted");
+    }
+
+    fn handle_start_event(
+        &mut self,
+        container_name: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        if self.is_running(&container_name) {
+            return Ok(());
+        }
+        self.register_task(container_name.clone());
+        let docker = self.docker.clone();
+        let powercap = self.powercap.clone();
+        let tasks = self.tasks.clone();
+        let filename = format!("{}.csv", container_name);
+        let path = self.path.join(&filename);
+        tokio::spawn(async {
+            if let Err(err) =
+                ContainerWatcher::execute(docker, powercap, tasks, container_name, path).await
+            {
+                eprintln!("container watcher errored: {:?}", err);
+            }
+        });
+        Ok(())
+    }
+
+    fn handle_event(
+        &mut self,
+        container_name: String,
+        action: Option<String>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        if !self.names.is_empty() && !self.names.contains(&container_name) {
+            return Ok(());
+        }
+        match action.as_ref().map(|action| action.as_str()) {
+            Some("start") => self.handle_start_event(container_name),
+            _ => Ok(()),
+        }
+    }
+
+    pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut filters = HashMap::new();
+        filters.insert("type", vec!["container"]);
+        let stream = &mut self.docker.events(Some(EventsOptions {
+            since: None,
+            until: None,
+            filters,
+        }));
+        while let Some(Ok(event)) = stream.next().await {
+            if let Some(container_name) = get_container_name(&event) {
+                println!(
+                    "received action {:?} for container {}",
+                    event.action, container_name
+                );
+                if let Err(err) = self.handle_event(container_name, event.action) {
+                    eprintln!("couldn't handle event: {:?}", err);
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Parser)]
+struct Params {
+    #[clap(
+        long,
+        about = "Name or ID of the container to monitor, separated by comma."
+    )]
+    containers: Option<String>,
+    #[clap(about = "Path to write the file")]
+    output: PathBuf,
+}
+
+/*
+impl Params {
+fn write_header(&self, file: &mut File) -> Result<(), Box<dyn std::error::Error>> {
+writeln!(
+file,
+"ts,pid_count,pid_limit,memory_usage,memory_limit,cpu_percent,cpu_count,cpu_energy",
+)?;
+Ok(())
+}
+
+async fn process_stream(
+&self,
+file: &mut File,
+docker: &Docker,
+powercap: Option<&PowerCap>,
+) -> Result<(), Box<dyn std::error::Error>> {
+let stream = &mut docker.stats(
+&self.container,
+Some(StatsOptions {
+stream: true,
+one_shot: false,
+}),
+);
+let mut last_energy: Option<u64> = None;
+while let Some(Ok(stat)) = stream.next().await {
+let energy = powercap.and_then(|pcap| pcap.intel_rapl.total_energy().ok());
+let delta = both(last_energy, energy).map(|(prev, next)| next - prev);
+last_energy = energy;
+let snap = Snapshot::build(stat, delta);
+writeln!(
+file,
+"{},{},{},{},{},{},{},{}",
+snap.ts,
+snap.pid_count.unwrap_or_default(),
+snap.pid_limit.unwrap_or_default(),
+snap.memory_usage.unwrap_or_default(),
+snap.memory_limit.unwrap_or_default(),
+snap.cpu_percent,
+snap.cpu_count,
+snap.cpu_energy.unwrap_or_default(),
+)?;
+}
+Ok(())
+}
+
+pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+let mut file = File::create(&self.output).expect("Couldn't create file");
+let docker =
+Docker::connect_with_local_defaults().expect("Couldn't connect to docker engine");
+let powercap = PowerCap::try_default().ok();
+self.write_header(&mut file)?;
+loop {
+self.process_stream(&mut file, &docker, powercap.as_ref())
+.await?;
+tokio::time::sleep(Duration::new(1, 0)).await;
+}
+}
+}
+*/
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let app = Application::parse();
-    app.run().await
+    let params = Params::parse();
+    let mut orchestrator = Orchestrator::try_from(params)?;
+    orchestrator.run().await
 }
