3ea1d11caeabab7d0cac777a5d688ed300413d75
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-11-06 16:34:10 +0100

    feat: record energy consumption for each container
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ use bollard::container::{Stats, StatsOptions};
 use bollard::Docker;
 use clap::Parser;
 use futures_util::stream::StreamExt;
+use powercap::PowerCap;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
+use std::time::Duration;
 
-#[derive(Parser)]
-struct Params {
-    #[clap(about = "Name or ID of the container to monitor")]
-    container: String,
-    #[clap(about = "Path to write the file")]
-    output: PathBuf,
+fn both<A, B>(a: Option<A>, b: Option<B>) -> Option<(A, B)> {
+    if let (Some(a), Some(b)) = (a, b) {
+        Some((a, b))
+    } else {
+        None
+    }
 }
 
 #[derive(Debug)]
@@ -23,10 +25,11 @@ struct Snapshot {
     memory_limit: Option<u64>,
     cpu_percent: f64,
     cpu_count: u64,
+    cpu_energy: Option<f64>,
 }
 
-impl From<Stats> for Snapshot {
-    fn from(item: Stats) -> Self {
+impl Snapshot {
+    fn build(item: Stats, total_cpu_energy: Option<u64>) -> Self {
         let cpu_delta =
             item.cpu_stats.cpu_usage.total_usage - item.precpu_stats.cpu_usage.total_usage;
         let system_delta = item.cpu_stats.system_cpu_usage.unwrap_or_default()
@@ -38,7 +41,9 @@ impl From<Stats> for Snapshot {
             .percpu_usage
             .map(|list| list.len() as u64)
             .unwrap_or(cpu_count) as f64;
-        let cpu_percent = (cpu_delta as f64 / system_delta as f64) * factor * 100.0;
+        let cpu_ratio = cpu_delta as f64 / system_delta as f64;
+        let cpu_percent = cpu_ratio * factor * 100.0;
+        let cpu_energy = total_cpu_energy.map(|value| value as f64 * cpu_ratio);
 
         Self {
             ts: item.read.timestamp(),
@@ -48,43 +53,79 @@ impl From<Stats> for Snapshot {
             memory_limit: item.memory_stats.limit,
             cpu_percent,
             cpu_count,
+            cpu_energy,
+        }
+    }
+}
+
+#[derive(Parser)]
+struct Application {
+    #[clap(about = "Name or ID of the container to monitor")]
+    container: String,
+    #[clap(about = "Path to write the file")]
+    output: PathBuf,
+}
+
+impl Application {
+    fn write_header(&self, file: &mut File) -> Result<(), Box<dyn std::error::Error>> {
+        writeln!(
+            file,
+            "ts,pid_count,pid_limit,memory_usage,memory_limit,cpu_percent,cpu_count,cpu_energy",
+        )?;
+        Ok(())
+    }
+
+    async fn process_stream(
+        &self,
+        file: &mut File,
+        docker: &Docker,
+        powercap: Option<&PowerCap>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let stream = &mut docker.stats(
+            &self.container,
+            Some(StatsOptions {
+                stream: true,
+                one_shot: false,
+            }),
+        );
+        let mut last_energy: Option<u64> = None;
+        while let Some(Ok(stat)) = stream.next().await {
+            let energy = powercap.and_then(|pcap| pcap.intel_rapl.total_energy().ok());
+            let delta = both(last_energy, energy).map(|(prev, next)| next - prev);
+            last_energy = energy;
+            let snap = Snapshot::build(stat, delta);
+            writeln!(
+                file,
+                "{},{},{},{},{},{},{},{}",
+                snap.ts,
+                snap.pid_count.unwrap_or_default(),
+                snap.pid_limit.unwrap_or_default(),
+                snap.memory_usage.unwrap_or_default(),
+                snap.memory_limit.unwrap_or_default(),
+                snap.cpu_percent,
+                snap.cpu_count,
+                snap.cpu_energy.unwrap_or_default(),
+            )?;
+        }
+        Ok(())
+    }
+
+    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut file = File::create(&self.output).expect("Couldn't create file");
+        let docker =
+            Docker::connect_with_local_defaults().expect("Couldn't connect to docker engine");
+        let powercap = PowerCap::try_default().ok();
+        self.write_header(&mut file)?;
+        loop {
+            self.process_stream(&mut file, &docker, powercap.as_ref())
+                .await?;
+            tokio::time::sleep(Duration::new(1, 0)).await;
         }
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let params = Params::parse();
-    let mut file = File::create(params.output).expect("Couldn't create file");
-    let docker = Docker::connect_with_local_defaults().unwrap();
-
-    let stream = &mut docker.stats(
-        &params.container,
-        Some(StatsOptions {
-            stream: true,
-            one_shot: false,
-        }),
-    );
-
-    writeln!(
-        file,
-        "ts,pid_count,pid_limit,memory_usage,memory_limit,cpu_percent,cpu_count",
-    )?;
-
-    while let Some(Ok(stat)) = stream.next().await {
-        let snap = Snapshot::from(stat);
-        writeln!(
-            file,
-            "{},{},{},{},{},{},{}",
-            snap.ts,
-            snap.pid_count.unwrap_or_default(),
-            snap.pid_limit.unwrap_or_default(),
-            snap.memory_usage.unwrap_or_default(),
-            snap.memory_limit.unwrap_or_default(),
-            snap.cpu_percent,
-            snap.cpu_count
-        )?;
-    }
-
-    Ok(())
+    let app = Application::parse();
+    app.run().await
 }
