51d08b1d653a2a498c2e501c168da3bacd36a961
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-12-01 18:34:57 +0100

    feat(enrichment): remove cpu energy when powercap feature not enabled
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/format/csv.rs
+++ src/format/csv.rs
@@ -3,6 +3,7 @@ use crate::model::Record;
 
 pub struct CsvFormatter;
 
+#[cfg(feature = "enrichment-powercap")]
 impl Formatter for CsvFormatter {
     fn format(&self, event: Record) -> Result<String, String> {
         Ok(format!(
@@ -17,6 +18,24 @@ impl Formatter for CsvFormatter {
             event.cpu_percent,
             event.cpu_count,
             event.cpu_energy.unwrap_or_default(),
+        ))
+    }
+}
+
+#[cfg(not(feature = "enrichment-powercap"))]
+impl Formatter for CsvFormatter {
+    fn format(&self, event: Record) -> Result<String, String> {
+        Ok(format!(
+            "{},{},{},{},{},{},{},{},{}",
+            event.container_id,
+            event.container_name,
+            event.ts,
+            event.pid_count.unwrap_or_default(),
+            event.pid_limit.unwrap_or_default(),
+            event.memory_usage.unwrap_or_default(),
+            event.memory_limit.unwrap_or_default(),
+            event.cpu_percent,
+            event.cpu_count,
         ))
     }
 }
